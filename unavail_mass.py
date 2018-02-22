a8f2974f8ab9af8e7c904e72002e89ab302f72bf
mvernacc <mvernacc@mit.edu>
2018-02-22 18:31:37 -0500

    Change to new formula for unavailable mass ratio, which includes partial re-use.

--- unavail_mass.py
+++ unavail_mass.py
@@ -1,73 +1,65 @@
 """Primary mission unavailable mass ratio for first stage recovery."""
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
 
 import payload
 
-def unavail_mass(chi_r, dv_r, E, c_1):
+def unavail_mass(a, P, z_m, E_1):
     """Primary mission unavailable mass ratio for first stage recovery.
 
     Arguments:
-        chi_r (scalar): ratio of recovery hardware mass to other 1st stage inert mass
-            [units: dimensionless].
-        dv_r (scalar): delta-V for the recovery maneuver [units: meter second**-1].
-        E (scalar): Structural mass ratio w/o reuse hardware [units: dimensionless].
-        c_1 (scalar): 1st stage effective exhaust velocity, e.g. Isp * g_0 
-            (average over recovery burns) [units: meter second**-1].
+        a (scalar): Fraction of the recovery vehicle dry mass which is added recovery
+            hardware [units: dimensionless].
+        P (scalar): Propulsion exponent recovery maneuver (e.g. Delta v / c) [units: dimensionless].
+        z_m (scalar): Fraction of baseline dry mass which is to be recovered [units: dimensionless].
+        E_1 (scalar): Structural mass ratio w/o reuse hardware [units: dimensionless].
 
     Returns:
         scalar: unavailable mass ratio [units: dimensionless].
     """
-    epsilon_1_prime = (chi_r + 1) / ((1 - E) / E + chi_r + 1) * np.exp(dv_r / c_1)
+    chi_r = a * z_m / (1 - a)
+    epsilon_1_prime = ((1 + chi_r + z_m / (1 - a) * (np.exp(P) - 1))
+        / (1 + chi_r + (1 - E_1) / E_1))
     return epsilon_1_prime
 
 
 def main():
-    chi_r = np.arange(0, 0.5, 1e-2)
-    dv_r = np.arange(0, 3500, 10)
-    chi_r_grid, dv_r_grid = np.meshgrid(chi_r, dv_r)
+    a = np.linspace(0, 0.99)
+    P = np.linspace(0, 1.5)
+    a_grid, P_grid = np.meshgrid(a, P)
 
-    # Stage exahust velocities [units: meter second**-1].
-    g_0 = 9.81
-    c_1 = 290 * g_0
-    c_2 = 350 * g_0
+    E_1 = 0.08
+    z_m = [1, 0.25]
 
-    E = 0.08
-    y = 0.10    # Stage mass ratio.
-    dv_mission = 12e3    # Mission delta-v [units: meter second**-1].
+    gs = gridspec.GridSpec(1, len(z_m))
 
-    e_grid = unavail_mass(chi_r_grid, dv_r_grid, E, c_1)
-
-    # Payload mass fraction for expendable rocket
-    pi_star_expend = payload.payload_fixed_stages(c_1, c_2, E, E, y, dv_mission)
-    print 'Expendable payload fraction = {:.3f}'.format(pi_star_expend)
-    
-    # Compute the payload mass fraction ratio for each recovery strategy
-    r_p_grid = np.zeros(chi_r_grid.shape)
-    for i in range(len(chi_r)):
-        for j in range(len(dv_r)):
-            pi_star = payload.payload_fixed_stages(c_1, c_2, e_grid[j, i], E,
-                                                   y, dv_mission)
-            r_p_grid[j, i] = pi_star / pi_star_expend
 
     # Plot results
-    plt.figure(figsize=(6,6))
-    cs = plt.contour(chi_r_grid, dv_r_grid, e_grid, 15)
-    plt.clabel(cs, inline=1, fontsize=10)
-    plt.xlabel('Recov. h/w mass ratio $\\chi_r = m_{rh,1}/m_{s,1}$ [-]')
-    plt.ylabel('Recov. $\\Delta v_r$ [m/s]')
-    plt.title("Unavailable mass ratio $\\epsilon_1'$\n" +
-              'for $c_1/g_0$={:.0f} s, $E$={:.2f}'.format(c_1/g_0, E))
+    plt.figure(figsize=(12,6))
+
+    for i in range(len(z_m)):
+        e_grid = unavail_mass(a_grid, P_grid, z_m[i], E_1)
+
+        plt.subplot(gs[0, i])
+        cs = plt.contour(a_grid, P_grid, e_grid, np.logspace(-1, 0, 15))
+        plt.clabel(cs, inline=1, fontsize=10)
+        plt.xlabel('Recov. h/w mass ratio $a = m_{rh,1}/(m_{rh,1} + m_{hv,1})$ [-]')
+        plt.ylabel('Propulsion exponent $P$ [-]')
+        plt.title("Unavailable mass ratio $\\epsilon_1'$\n"
+                  + 'for $E_1$={:.2f}, $z_m$={:.2f}'.format(E_1, z_m[i]))
+
+    plt.tight_layout()
     plt.savefig('unavail_mass.png')
 
-    plt.figure(figsize=(6,6))
-    cs = plt.contour(chi_r_grid, dv_r_grid, r_p_grid)
-    plt.clabel(cs, inline=1, fontsize=10)
-    plt.xlabel('Recov. h/w mass ratio $\\chi_r = m_{rh,1}/m_{s,1}$ [-]')
-    plt.ylabel('Recov. $\\Delta v_r$ [m/s]')
-    plt.title("Recov. / Expend payload ratio $r_p$\n" +
-              'for $c_1/g_0$={:.0f} s, $c_2/g_0$={:.0f} s $E_1=E_2$={:.2f}, $\\Delta v_*$={:.0f} m/s'.format(
-                c_1/g_0, c_2/g_0, E, dv_mission))
+    # plt.figure(figsize=(6,6))
+    # cs = plt.contour(chi_r_grid, dv_r_grid, r_p_grid)
+    # plt.clabel(cs, inline=1, fontsize=10)
+    # plt.xlabel('Recov. h/w mass ratio $\\chi_r = m_{rh,1}/m_{s,1}$ [-]')
+    # plt.ylabel('Recov. $\\Delta v_r$ [m/s]')
+    # plt.title("Recov. / Expend payload ratio $r_p$\n" +
+    #           'for $c_1/g_0$={:.0f} s, $c_2/g_0$={:.0f} s $E_1=E_2$={:.2f}, $\\Delta v_*$={:.0f} m/s'.format(
+    #             c_1/g_0, c_2/g_0, E, dv_mission))
     plt.show()
 
 
