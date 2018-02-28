d0d38be2712f31eb6f720e64f5dde4abbe827e25
mvernacc <mvernacc@mit.edu>
2018-02-27 23:41:16 -0500

    Plot comparison of multiple launch vehicles.

--- stage_mass_ratio.py
+++ stage_mass_ratio.py
@@ -7,12 +7,44 @@ from itertools import cycle
 import payload
 import launch_vehicles
 
+g_0 = 9.81
+
+def get_dv_mission(mission):
+    """Mission delta-v [units: meter second**-1]"""
+    if mission == 'GTO':
+        return 12e3    # GTO
+    elif mission == 'LEO':
+        return 9.5e3    # LEO
+    else:
+        raise ValueError()
+
 
 def main():
-    g_0 = 9.81
+    mission = 'GTO'
 
-    lv = launch_vehicles.atlas_v_401
+    # Launch vehicles to examine
+    lvs = (launch_vehicles.delta_iv_m, launch_vehicles.atlas_v_401, launch_vehicles.f9_b3_e)
+    
+    fig, axes = plt.subplots(ncols=len(lvs), sharey=True)
+    fig.set_size_inches(12, 6)
 
+    for i in range(len(lvs)):
+        plt.sca(axes[i])
+        plot_lv(lvs[i], mission, full_legend=(i==2))
+        if i > 0:
+            plt.ylabel('')
+
+    plt.suptitle("Effect of $\\epsilon_1'$ on optimal stage mass ratio"
+        + ' for {:s} ($\Delta v_*$ = {:.1f} km/s)'.format(
+            mission, get_dv_mission(mission) * 1e-3))
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0, top=0.85)
+    plt.show()
+
+
+def plot_lv(lv, mission, full_legend=True):
+    """Plot the payload capacity vs. stage mass ratio for a launch vehicle."""
     # Stage exahust velocities [units: meter second**-1].
     # O2/kerosene
     c_1 = lv.c_1
@@ -26,13 +58,8 @@ def main():
     # Stage mass ratio [units: dimensionless].
     y = np.linspace(0.05, 0.50)
 
-    # Mission delta-v [units: meter second**-1].
-    mission = 'GTO'
-    dv_mission = 0
-    if mission == 'GTO':
-        dv_mission = 12e3    # GTO
-    elif mission == 'LEO':
-        dv_mission = 9.5e3    # LEO
+    # Mission delta-v [units: meter second**-1]
+    dv_mission = get_dv_mission(mission)
 
     #colors
     color_cycle = cycle(['C' + str(i) for i in range(len(e_1_steps))])
@@ -45,9 +72,12 @@ def main():
         if e_1 == e_1_actual:
             color='black'
             label="$\\epsilon_1'=${:.2f} (actual)".format(e_1)
-        else:
+        elif full_legend:
             color = next(color_cycle)
             label="$\\epsilon_1'=${:.2f}".format(e_1)
+        else:
+            color = next(color_cycle)
+            label = None
 
         plt.plot(y, pi_star, label=label, color=color)
 
@@ -66,14 +96,19 @@ def main():
     plt.legend()
     plt.xlabel("2nd/1st stage mass ratio $y$ [-]")
     plt.ylabel('Payload mass fraction $\\pi_*$ [-]')
-    plt.title("Effect of $\\epsilon_1'$ on optimal stage mass ratio\n"
-        + 'for $c_1/g_0$={:.0f} s, $c_2/g_0$={:.0f} s,'.format(c_1/g_0, c_2/g_0)
-        + '$\\epsilon_2$={:.2f}, $\Delta v_*$={:.1f} km/s'.format(e_2, dv_mission * 1e-3))
+    plt.title('$c_1/g_0$={:.0f} s, $c_2/g_0$={:.0f} s\n'.format(c_1 / g_0, c_2 / g_0)
+        + '$\\epsilon_2$={:.2f}'.format(e_2))
+
     # Start plot at 0,0
     ax = plt.gca()
-    ax.set_xlim(xmin=0)
+    ax.set_xlim([0, 0.49])
     ax.set_ylim(ymin=0)
-    plt.show()
+
+    # Consistent pi_star max across all subplots
+    if mission == 'GTO':
+        ax.set_ylim(ymax=0.02)
+    elif mission == 'LEO':
+        ax.set_ylim(ymax=0.04)
 
 
 if __name__ == '__main__':
