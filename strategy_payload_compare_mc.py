be773c8bf8129c90560ba146a721469d2d7ce391
Matthew Vernacchia <mvernacc@mit.edu>
2018-07-23 14:28:07 -0400

    Add technology uncertainties.

--- strategy_payload_compare_mc.py
+++ strategy_payload_compare_mc.py
@@ -25,11 +25,20 @@ def monte_carlo():
     dv_prop_ls = np.array([2930 + 800 + 150, 4360 + 800 + 350])
     # Assume kerosene technology
     tech = kerosene_tech
+    tech_uncerts = [
+        rdm.UniformUncertainty('c_1', min_value=260 * g_0, max_value=310 * g_0),
+        rdm.UniformUncertainty('c_2', min_value=330 * g_0, max_value=350 * g_0),
+        rdm.UniformUncertainty('E_1', min_value=0.04, max_value=0.08),
+        rdm.UniformUncertainty('E_2', min_value=0.03, max_value=0.06),
+        rdm.UniformUncertainty('y', min_value=0.2, max_value=0.3),
+    ]
+
+    c_1_recov = 260 * g_0 # TODO
 
     strategy_uncerts = {
         'prop_ls': [
             rdm.UniformUncertainty('a', min_value=0.05, max_value=0.07),
-            rdm.UniformUncertainty('P', min_value=dv_prop_ls[0] / tech.c_1, max_value=dv_prop_ls[1] / tech.c_1)
+            rdm.UniformUncertainty('P', min_value=dv_prop_ls[0] / c_1_recov, max_value=dv_prop_ls[1] / c_1_recov)
         ],
         'wing_ls': [
             rdm.UniformUncertainty('a', min_value=0.28, max_value=0.52),
@@ -42,7 +51,7 @@ def monte_carlo():
         ],
         'prop_dr': [
             rdm.UniformUncertainty('a', min_value=0.05, max_value=0.07),
-            rdm.UniformUncertainty('P', min_value=800 / tech.c_1, max_value=1150 / tech.c_1)
+            rdm.UniformUncertainty('P', min_value=800 / c_1_recov, max_value=1150 / c_1_recov)
         ],
         'wing_dr': [
             rdm.UniformUncertainty('a', min_value=0.18, max_value=0.37),
@@ -53,52 +62,62 @@ def monte_carlo():
         ],
     }
 
-    def perf_model_func(a=0, P=0, z_m=1):
-        e_1 = unavail_mass.unavail_mass(a=a, P=P, z_m=z_m, E_1=tech.E_1)
+
+    def perf_model_func(a=0, P=0, z_m=1, c_1=297*g_0, E_1=0.060, c_2=350*g_0, E_2=0.040, y=0.265):
+        e_1 = unavail_mass.unavail_mass(a=a, P=P, z_m=z_m, E_1=E_1)
         pld_frac_recov = payload.payload_fixed_stages(
-            c_1=tech.c_1,
-            c_2=tech.c_2,
+            c_1=c_1,
+            c_2=c_2,
             e_1=e_1,
-            e_2=tech.E_2,
-            y=tech.y,
+            e_2=E_2,
+            y=y,
             dv_mission=dv_mission
             )
-        return pld_frac_recov
+        pld_frac_expend = payload.payload_fixed_stages(
+            c_1=c_1,
+            c_2=c_2,
+            e_1=E_1,
+            e_2=E_2,
+            y=y,
+            dv_mission=dv_mission
+            )
+        r_p = pld_frac_recov / pld_frac_expend
+        return pld_frac_recov, r_p
+
 
     perf_model = rdm.Model(perf_model_func)
 
     perf_model.parameters = [
         rdm.Parameter('a'),
         rdm.Parameter('P'),
-        rdm.Parameter('z_m')
+        rdm.Parameter('z_m'),
+        rdm.Parameter('c_1'),
+        rdm.Parameter('c_2'),
+        rdm.Parameter('E_1'),
+        rdm.Parameter('E_2'),
+        rdm.Parameter('y'),
     ]
 
     perf_model.responses = [
-        rdm.Response('pld_frac_recov', rdm.Response.MAXIMIZE)
+        rdm.Response('pld_frac_recov', rdm.Response.MAXIMIZE),
+        rdm.Response('r_p', rdm.Response.MAXIMIZE),
     ]
 
-    results_dict = {}
+    pld_frac_recov = {}
+    r_p = {}
 
     for strat, uncerts in strategy_uncerts.items():
-        perf_model.uncertainties = uncerts
+        perf_model.uncertainties = uncerts + tech_uncerts
 
         scenarios = rdm.sample_lhs(perf_model, nsamples=1000)
 
         results = rdm.evaluate(perf_model, scenarios)
-        results_dict[strat] = results['pld_frac_recov']
 
-    pld_frac_recov = pandas.DataFrame(results_dict)
+        pld_frac_recov[strat] = results['pld_frac_recov']
+        r_p[strat] = results['r_p']
 
-    pld_frac_expend = payload.payload_fixed_stages(
-        c_1=tech.c_1,
-        c_2=tech.c_2,
-        e_1=tech.E_1,
-        e_2=tech.E_2,
-        y=tech.y,
-        dv_mission=dv_mission
-        )
-
-    r_p = pld_frac_recov / pld_frac_expend
+    pld_frac_recov = pandas.DataFrame(pld_frac_recov)
+    r_p = pandas.DataFrame(r_p)
 
     sns.violinplot(data=r_p, width=1)
 
