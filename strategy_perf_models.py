aac5da0499f97af5ad29672b7c511c4d74a9ae27
Matthew Vernacchia <mvernacc@mit.edu>
2018-08-24 12:04:26 -0400

    Break technology into separate objects for 1st and 2nd stage.

--- strategy_perf_models.py
+++ strategy_perf_models.py
@@ -15,29 +15,48 @@ import launch_vehicles
 
 g_0 = 9.81
 
-Technology = namedtuple('Technology', ['fuel', 'cycle', 'uncertainties'])
+Technology = namedtuple('Technology', ['fuel', 'cycle', 'stage', 'uncertainties'])
 
-kero_GG_tech = Technology(
+kero_GG_boost_tech = Technology(
     fuel='kerosene',
     cycle='gas generator',
+    stage='booster',
     uncertainties=[
         rdm.TriangularUncertainty('c_1', min_value=264.6 * g_0, mode_value=287.9 * g_0, max_value=305.4 * g_0),
-        rdm.TriangularUncertainty('c_2', min_value=335.2 * g_0, mode_value=345.0 * g_0, max_value=348.0 * g_0),
         rdm.TriangularUncertainty('E_1', min_value=0.055, mode_value=0.060, max_value=0.065),
+    ]
+    )
+
+kero_GG_upper_tech = Technology(
+    fuel='kerosene',
+    cycle='gas generator',
+    stage='upper',
+    uncertainties=[
+        rdm.TriangularUncertainty('c_2', min_value=335.2 * g_0, mode_value=345.0 * g_0, max_value=348.0 * g_0),
         rdm.TriangularUncertainty('E_2', min_value=0.040, mode_value=0.050, max_value=0.060),
     ]
     )
 
-H2_SC_tech = Technology(
+H2_SC_boost_tech = Technology(
     fuel='H2',
     cycle='staged combustion',
+    stage='booster',
     uncertainties=[
         rdm.TriangularUncertainty('c_1', min_value=369.5 * g_0, mode_value=398.1 * g_0, max_value=418.7 * g_0),
-        rdm.TriangularUncertainty('c_2', min_value=444.9 * g_0, mode_value=458.0 * g_0, max_value=462.0 * g_0),
         rdm.TriangularUncertainty('E_1', min_value=0.11, mode_value=0.12, max_value=0.13),
+    ]
+    )
+
+H2_SC_upper_tech = Technology(
+    fuel='H2',
+    cycle='staged combustion',
+    stage='upper',
+    uncertainties=[
+        rdm.TriangularUncertainty('c_2', min_value=444.9 * g_0, mode_value=458.0 * g_0, max_value=462.0 * g_0),
         rdm.TriangularUncertainty('E_2', min_value=0.080, mode_value=0.085, max_value=0.090),
     ]
     )
+
 
 Mission = namedtuple('Mission', ['name', 'dv'])
 
@@ -65,13 +84,15 @@ z_m_uncert = rdm.UniformUncertainty('z_m', min_value=0.15, mode_value=0.25, max_
 class Strategy(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, landing_method, recovery_location, portion_recovered, tech, mission):
+    def __init__(self, landing_method, recovery_location, portion_recovered, tech_1, tech_2, mission):
         self.landing_method = landing_method
         self.recovery_location = recovery_location
         self.portion_recovered = portion_recovered
-        self.tech = tech
+        self.tech_1 = tech_1
+        self.tech_2 = tech_2
         self.uncertainties = []
-        self.uncertainties += tech.uncertainties
+        self.uncertainties += tech_1.uncertainties
+        self.uncertainties += tech_2.uncertainties
         self.perf_model = rdm.Model(self.evaluate_performance)
         self.mission = mission
 
@@ -100,9 +121,10 @@ class Strategy(object):
 class StrategyNoPropulsion(Strategy):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, landing_method, recovery_location, portion_recovered, tech, mission):
+    def __init__(self, landing_method, recovery_location, portion_recovered, 
+                 tech_1, tech_2, mission):
         super(StrategyNoPropulsion, self).__init__(landing_method, recovery_location,
-                                                   portion_recovered, tech, mission)
+                                                   portion_recovered, tech_1, tech_2, mission)
 
     def evaluate_performance(self, c_1, c_2, E_1, E_2, a, z_m=1):
         e_1 = unavail_mass(a, P=0, z_m=z_m, E_1=E_1)
@@ -112,8 +134,8 @@ class StrategyNoPropulsion(Strategy):
 
 class Expendable(Strategy):
 
-    def __init__(self, tech, mission, y=0.20):
-        super(Expendable, self).__init__('expendable', 'N/A', 'none', tech, mission)
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
+        super(Expendable, self).__init__('expendable', 'N/A', 'none', tech_1, tech_2, mission)
         self.y = y
         self.setup_model()
 
@@ -123,8 +145,9 @@ class Expendable(Strategy):
 
 class PropulsiveLaunchSite(Strategy):
 
-    def __init__(self, tech, mission, y=0.20):
-        super(PropulsiveLaunchSite, self).__init__('propulsive', 'launch site', 'full', tech, mission)
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
+        super(PropulsiveLaunchSite, self).__init__('propulsive', 'launch site', 'full',
+                                                   tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.09, mode_value=0.14, max_value=0.19),
@@ -142,8 +165,9 @@ class PropulsiveLaunchSite(Strategy):
 
 
 class WingedPoweredLaunchSite(Strategy):
-    def __init__(self, tech, mission, y=0.20):
-        super(WingedPoweredLaunchSite, self).__init__('winged powered', 'launch site', 'full', tech, mission)
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
+        super(WingedPoweredLaunchSite, self).__init__('winged powered', 'launch site', 'full',
+                                                      tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.490, mode_value=0.574, max_value=0.650),
@@ -161,9 +185,9 @@ class WingedPoweredLaunchSite(Strategy):
 
 
 class WingedPoweredLaunchSitePartial(Strategy):
-    def __init__(self, tech, mission, y=0.20):
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
         super(WingedPoweredLaunchSitePartial, self).__init__('winged powered', 'launch site', 'partial',
-                                                      tech, mission)
+                                                      tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.490, mode_value=0.574, max_value=0.650),
@@ -182,9 +206,9 @@ class WingedPoweredLaunchSitePartial(Strategy):
 
 class PropulsiveDownrange(Strategy):
 
-    def __init__(self, tech, mission, y=0.20):
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
         super(PropulsiveDownrange, self).__init__('propulsive', 'downrange', 'full',
-                                                   tech, mission)
+                                                   tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.09, mode_value=0.14, max_value=0.19),
@@ -205,9 +229,9 @@ class PropulsiveDownrange(Strategy):
 
 
 class WingedGlider(StrategyNoPropulsion):
-    def __init__(self, tech, mission, y=0.20):
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
         super(WingedGlider, self).__init__('winged glider', 'downrange', 'full',
-                                                   tech, mission)
+                                           tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.380, mode_value=0.426, max_value=0.540),
@@ -216,9 +240,9 @@ class WingedGlider(StrategyNoPropulsion):
 
 
 class Parachute(StrategyNoPropulsion):
-    def __init__(self, tech, mission, y=0.20):
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
         super(Parachute, self).__init__('parachute', 'downrange', 'full',
-                                                   tech, mission)
+                                        tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.15, mode_value=0.17, max_value=0.19),
@@ -227,9 +251,9 @@ class Parachute(StrategyNoPropulsion):
 
 
 class ParachutePartial(StrategyNoPropulsion):
-    def __init__(self, tech, mission, y=0.20):
+    def __init__(self, tech_1, tech_2, mission, y=0.20):
         super(ParachutePartial, self).__init__('parachute', 'downrange', 'partial',
-                                                   tech, mission)
+                                               tech_1, tech_2, mission)
         self.y = y
         self.uncertainties += [
             rdm.TriangularUncertainty('a', min_value=0.15, mode_value=0.17, max_value=0.19),
@@ -243,14 +267,15 @@ def demo():
                 WingedPoweredLaunchSite, WingedPoweredLaunchSitePartial,
                 PropulsiveDownrange, WingedGlider, Parachute, ParachutePartial]
 
-    for tech in [kero_GG_tech, H2_SC_tech]:
+    for tech_1, tech_2 in zip([kero_GG_boost_tech, H2_SC_boost_tech],
+                              [kero_GG_upper_tech, H2_SC_upper_tech]):
         for mission in [LEO, GTO]:
             plt.figure(figsize=(10, 6))
             
             results = {}
             xticks = []
             for strat in strats:
-                strat_instance = strat(tech, mission)
+                strat_instance = strat(tech_1, tech_2, mission)
                 name = strat.__name__
                 res = strat_instance.sample_perf_model(nsamples=100)
                 res = res.as_dataframe()
@@ -267,7 +292,9 @@ def demo():
 
             ax = sns.violinplot(data=pi_star)
             plt.ylabel('Overall payload mass fraction $\\pi_*$ [-]')
-            plt.title('{:s} mission, {:s} {:s} technology'.format(mission.name, tech.fuel, tech.cycle))
+            plt.title('Payload performance distributions for {:s} mission'.format(mission.name)
+                + '\nstage 1: {:s} {:s} tech.,'.format(tech_1.fuel, tech_1.cycle)
+                + ' stage 2: {:s} {:s} tech.'.format(tech_2.fuel, tech_2.cycle))
             plt.ylim([0, plt.ylim()[1]])
 
             ax.set_xticklabels(xticks)
@@ -278,7 +305,8 @@ def demo():
             plt.axvline(x=3.5, color='grey')
             plt.text(x=4, y=0.0, s='Downrange recovery')
 
-            if tech.fuel == 'kerosene' and tech.cycle == 'gas generator':
+            if (tech_1.fuel == 'kerosene' and tech_1.cycle == 'gas generator'
+                and tech_2.fuel == 'kerosene' and tech_2.cycle == 'gas generator'):
                 # Plot Falcon 9 data for comparison
                 pi_star = launch_vehicles.f9_b3_e.payload_actual(mission.name)
                 plt.scatter(0, pi_star, marker='+', color='red', zorder=10)
@@ -296,7 +324,7 @@ def demo():
 
             plt.tight_layout()
             plt.savefig(os.path.join('plots', 'strategy_perf_{:s}_{:s}.png'.format(
-                mission.name, tech.fuel)))
+                mission.name, tech_1.fuel)))
     plt.show()
 
 
