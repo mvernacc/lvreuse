1505dfe338ce4164a84473bd4d39ca6bd5eb92ff
Kelly Mathesius <kjmath@mit.edu>
2018-08-28 09:40:02 -0400

    added interpolation for ioc for use with non-integer launch rates

--- indirect_ops.py
+++ indirect_ops.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def indirect_ops_cost(launch_rate, launch_provider_type):
 
     ioc_data = {'A': [65, 49, 42, 38, 35, 33, 31, 30, 29, 27, 26, 25],
@@ -5,6 +7,8 @@ def indirect_ops_cost(launch_rate, launch_provider_type):
                 'C': [32, 24, 22, 19, 18, 17, 16, 15, 14, 13, 12, 11]}
 
     ioc_line = ioc_data[launch_provider_type]
-    ioc_cost = ioc_line[launch_rate - 1]
+    launch_rate_list = range(1, 13)
+
+    ioc_cost = np.interp(launch_rate, launch_rate_list, ioc_line)
 
     return ioc_cost
